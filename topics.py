e6907d6848211972dd18311803d876f6570670db
Ed Summers <ehs@pobox.com>
2015-04-10 17:26:57 -0400

    start of some simple topic modeling - i hope

--- topics.py
+++ topics.py
@@ -1,18 +1,46 @@
 #!/usr/bin/env python
 
+import os
+import re
 import json
-from sklearn.feature_extraction.text import CountVectorizer
 
-tweets = []
-for line in open("tweets.json"):
-    tweet = json.loads(line)
-    if not 'retweeted_status' in tweet and tweet['lang'] == 'en':
-        tweets.append(tweet['text'].lower())
+from itertools import imap
+from gensim import corpora, models, similarities
 
-stop_words = ["http", "https", "rt", "@", ":"]
+def docs():
+    for line in open("tweets.json"):
+        tweet = json.loads(line)
+        if not 'retweeted_status' in tweet and tweet['lang'] == 'en':
+            yield words(tweet['text'])
 
-vectorizer = CountVectorizer(stop_words=stop_words)
-vectorizer.fit(tweets)
+def words(s):
+    words = s.lower().split(' ')
+    new_words = []
+    for word in words:
+        if re.match(r'(@|http|#)', word):
+            continue
+        new_words.append(word)
+    return new_words
 
-names = vectorizer.get_feature_names()
-print names
+def get_dictionary(path):
+    if not os.path.isfile(path):
+        dictionary = corpora.Dictionary()
+        dictionary.save(path)
+    else:
+        dictionary = corpora.Dictionary.load(path)
+    return dictionary
+
+def get_corpus(path, dictionary):
+    def ids():
+        for doc in docs():
+            yield dictionary.doc2bow(doc)
+    if not os.path.isfile(path):
+        corpus = corpora.MmCorpus.serialize(path, ids())
+    else:
+        corpus = corpora.MmCorpus(path)
+    return corpus
+
+dictionary = get_dictionary("topics.dict")
+corpus = get_corpus("topics.mm", dictionary)
+
+print corpus
