4906e238c78309a7673ff026640f542d2d13152d
Ed Summers <ehs@pobox.com>
2015-04-23 15:09:17 -0400

    got topic modeling working

--- topics.py
+++ topics.py
@@ -3,27 +3,48 @@
 import os
 import re
 import json
+import string
+import argparse
 
 from gensim import corpora, models, similarities
 
+punctuation = set(string.punctuation)
+
 def docs():
+    stop_words = get_stop_words('stopwords.txt')
     for line in open("tweets.json"):
         tweet = json.loads(line)
         if not 'retweeted_status' in tweet and tweet['lang'] == 'en':
-            yield words(tweet['text'])
+            yield words(tweet['text'], stop_words)
 
-def words(s):
+def words(s, stop_words):
     words = s.lower().split(' ')
     new_words = []
     for word in words:
-        if re.match(r'(@|http|#)', word):
+        word = word.strip()
+        if not word:
             continue
+        if word in stop_words:
+            continue
+        if re.match(r'^(@|\#|http)', word):
+            continue
+        if len(word) < 4:
+            continue
+
+        word = ''.join(ch for ch in word if ch not in punctuation)
         new_words.append(word)
     return new_words
 
+def get_stop_words(path):
+    stops = {}
+    for word in open('stop_words.txt'):
+        word = word.strip().lower()
+        stops[word] = True
+    return stops
+
 def get_dictionary(path):
     if not os.path.isfile(path):
-        dictionary = corpora.Dictionary()
+        dictionary = corpora.Dictionary(docs())
         dictionary.save(path)
     else:
         dictionary = corpora.Dictionary.load(path)
@@ -39,7 +60,23 @@ def get_corpus(path, dictionary):
         corpus = corpora.MmCorpus(path)
     return corpus
 
-dictionary = get_dictionary("topics.dict")
-corpus = get_corpus("topics.mm", dictionary)
+def main():
+    parser = argparse.ArgumentParser(description="topic model some tweets")
+    parser.add_argument('--num_topics', type=int, help='number of topics', default=20)
+    parser.add_argument('--num_words', type=int, help='number of words', default=20)
+    args = parser.parse_args()
 
-print corpus
+    dictionary = get_dictionary("topics.dict")
+    corpus = get_corpus("topics.mm", dictionary)
+    lda = models.ldamodel.LdaModel(corpus, id2word=dictionary, num_topics=args.num_topics)
+
+    count = 0
+    for topic in lda.show_topics(num_topics=args.num_topics, num_words=args.num_words, formatted=False):
+        count += 1 
+        print "topic #%i" % count
+        for score, label in topic:
+            print "- %s" % label.strip()
+        print
+
+if __name__ == "__main__":
+    main()
